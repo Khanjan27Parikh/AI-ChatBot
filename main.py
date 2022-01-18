be08bff8d95d81c48db6a1090119e7c29f1b8c90
Khanjan Parikh <developerkhanju09@gmail.com>
2022-01-18 20:00:06 +0530

    training model

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy
+import numpy as np
 import tflearn
 import tensorflow
 import random
@@ -14,13 +14,45 @@ with open("intents.json") as file:
 
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent["tags"])
 
     if intent["tags"] not in labels:
         labels.append(intent["tags"])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+    
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = np.array(training)
+output = np.array(output)
